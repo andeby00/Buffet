9b78d4f1b252a8e52788a9d9271f5539a8e73288
aspschou <57791851+aspschou@users.noreply.github.com>
2021-04-25 15:55:37 +0200

    Added models

---
+++ Buffet/Models/Reception.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Models
+{
+    public class Reception
+    {
+        //Dato
+        public DateTime Date { get; set; }
+
+        //Antal Voksne
+        public int Adults { get; set; }
+
+        //Antal Børn
+        public int Children { get; set; }
+    }
+}
---
+++ Buffet/Models/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Models
+{
+    public class Reservation
+    {
+        //Dato
+        public DateTime Date { get; set; }
+
+        //Værelsesnummer
+        public string RoomNumber { get; set; }
+
+        //Antal Voksne
+        public int Adults { get; set; }
+
+        //Antal Børn
+        public int Children { get; set; }
+    }
+}
