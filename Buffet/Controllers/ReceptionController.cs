444f92146718bf161515b4cf078a81258b84dea0
Rasmus <rasmuswium@hotmail.com>
2021-04-26 20:40:17 +0200

    Kitchen done ish ? ? nu reception ahahaha

--- Buffet/Controllers/ReceptionController.cs
+++ Buffet/Controllers/ReceptionController.cs
@@ -4,13 +4,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Buffet.Data;
+using Buffet.Models;
 
 namespace Buffet.Controllers
 {
     public class ReceptionController : Controller
     {
-        [Authorize(Policy = "IsKitchen")]
+         ApplicationDbContext _context;
+
+        public ReceptionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize(Policy = "IsReception")]
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Policy = "IsReception")]
+        public async Task<IActionResult> Create([Bind("Date,Adults,Children")] Reservation reservation)
         {
             return View();
         }
