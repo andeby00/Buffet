c204abf1b372d43e748ddd6c847260bcaa76b244
aspschou <57791851+aspschou@users.noreply.github.com>
2021-04-26 19:10:49 +0200

    Fixed Db names

---
+++ Buffet/Data/Migrations/20210426171032_FixedNames.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Buffet.Data.Migrations
+{
+    public partial class FixedNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "receptions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_reservations",
+                table: "reservations");
+
+            migrationBuilder.DropColumn(
+                name: "RoomNumber",
+                table: "reservations");
+
+            migrationBuilder.RenameTable(
+                name: "reservations",
+                newName: "Reservations");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Reservations",
+                table: "Reservations",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "CheckedIns",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(nullable: false),
+                    RoomNumber = table.Column<string>(nullable: true),
+                    Adults = table.Column<int>(nullable: false),
+                    Children = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CheckedIns", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CheckedIns");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Reservations",
+                table: "Reservations");
+
+            migrationBuilder.RenameTable(
+                name: "Reservations",
+                newName: "reservations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RoomNumber",
+                table: "reservations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_reservations",
+                table: "reservations",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "receptions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Adults = table.Column<int>(type: "int", nullable: false),
+                    Children = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_receptions", x => x.Id);
+                });
+        }
+    }
+}
